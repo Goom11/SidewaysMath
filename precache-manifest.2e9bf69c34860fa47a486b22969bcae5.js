9a3be8b40f3b4013389e5e9529b58a9c2298ab68
Aman Agarwal <amanaamazing@gmail.com>
2020-05-06 15:11:25 -0700

    Updates

---
+++ precache-manifest.2e9bf69c34860fa47a486b22969bcae5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5de4d40b2fa728eca541509cce1868f",
+    "url": "/SidewaysMath/index.html"
+  },
+  {
+    "revision": "98cea6fe7f22f08cbb16",
+    "url": "/SidewaysMath/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "692280320011045f72b5",
+    "url": "/SidewaysMath/static/js/2.486d8f95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SidewaysMath/static/js/2.486d8f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98cea6fe7f22f08cbb16",
+    "url": "/SidewaysMath/static/js/main.9f1071dc.chunk.js"
+  },
+  {
+    "revision": "77622c2747bac2496fb8",
+    "url": "/SidewaysMath/static/js/runtime-main.9ef8e8f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SidewaysMath/static/media/logo.5d5d9eef.svg"
+  }
+]);
